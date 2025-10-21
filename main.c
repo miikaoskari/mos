7dc5d81f84b35475f1c879183cdd51da84860e90
miikaoskari <miika.karjalainen13@gmail.com>
2025-10-21 19:17:24 +0300

    Initial project commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdint.h>
+
+volatile uint8_t *uart = (uint8_t *) 0x09000000;
+
+void putchar(char c) {
+    *uart = c;
+}
+
+void print(const char *s) {
+    while(*s != '\0') {
+        putchar(*s);
+        s++;
+    }
+}
+
+void kmain(uint64_t dtb_ptr32, uint64_t x1, uint64_t x2, uint64_t x3) {
+    print("Hello world!\n");
+}
