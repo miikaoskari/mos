686d47544547263bdae780d2ef81a1ea5f83caa3
miikaoskari <miika.karjalainen13@gmail.com>
2025-11-11 23:02:33 +0200

    Call probe all drivers from fdt

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdint.h>
+#include <libfdt.h>
+#include "driver.h"
 
 volatile uint8_t *uart = (uint8_t *) 0x09000000;
 
@@ -6,15 +8,21 @@ void putchar(char c) {
     *uart = c;
 }
 
-void print(const char *s) {
+void print(const char *s)
+{
     while(*s != '\0') {
         putchar(*s);
         s++;
     }
 }
 
-void kmain(uint64_t dtb_ptr32, uint64_t x1, uint64_t x2, uint64_t x3) {
+void kmain(uint64_t dtb_ptr32, uint64_t x1, uint64_t x2, uint64_t x3)
+{
     void *fdt = (void *)(uintptr_t)dtb_ptr32;
 
-    uint32_t *magic = (uint32_t *)fdt;
+    probe_all_drivers_from_fdt(fdt);
+
+    for(;;) {
+        // loop forever!
+    }
 }
