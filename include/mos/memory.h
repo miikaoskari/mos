118683e288724438a0b60a9b92fcf91e8e1e48d1
miikaoskari <miika.karjalainen13@gmail.com>
2025-12-08 17:41:25 +0200

    allocation and initialization for buddy

--- include/mos/memory.h
+++ include/mos/memory.h
@@ -1,12 +1,21 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+#include <stdint.h>
+
 #define PAGE_SIZE 4096UL
 #define RAM_START 0x40000000ULL
 #define RAM_SIZE 0x08000000ULL
 #define MAX_ORDER 11U
 #define MIN_ORDER 0
 
+struct page {
+    struct page* next;
+    unsigned int order;
+};
+
 void phys_mem_init(uint64_t phys_ram_start, uint64_t phys_ram_size, uint64_t kernel_start_addr, uint64_t kernel_end_addr);
+struct page* alloc_pages(const unsigned int order);
+void free_pages(struct page* page);
 
 #endif // MEMORY_H
--- src/memory.c
+++ src/memory.c
@@ -1,14 +1,27 @@
 #include "mos/memory.h"
 #include "string.h"
 #include <stdint.h>
+#include <stdlib.h>
 
-struct page {
-    struct page* next;
-    int order;
-};
+/**
+ * | order | size    |
+ * |-------|---------|
+ * | 0     | 4kb     |
+ * | 1     | 8kb     |
+ * | 2     | 16kb    |
+ * | 3     | 32kb    |
+ * | 4     | 64kb    |
+ * | 5     | 128kb   |
+ * | 6     | 256kb   |
+ * | 7     | 512kb   |
+ * | 8     | 1024kb  |
+ * | 9     | 2048kb  |
+ * | 10    | 4096kb  |
+ * | 11    | 8192kb  |
+ */
 
 // mem_map uses some random number. should be ram size / page size
-struct page *mem_map[4096];
+struct page mem_map[4096];
 
 // free_list[x] contains all free order x
 struct page *free_list[MAX_ORDER + 1];
@@ -18,47 +31,63 @@ void phys_mem_init(uint64_t phys_ram_start, uint64_t phys_ram_size, uint64_t ker
     memset(mem_map, 0, sizeof(mem_map));
     memset(free_list, 0, sizeof(free_list));
 
+    struct page *initial_block = mem_map;
+    initial_block->order = MAX_ORDER;
+    initial_block->next = NULL;
+
+    free_list[MAX_ORDER] = initial_block;
+
     return;
 }
 
 struct page* alloc_pages(const unsigned int order)
 {
     if (order > MAX_ORDER) {
-        return NULL;
+        return nullptr;
     }
 
-    struct page* free_block = NULL;
+    struct page *free_block = nullptr;
+    unsigned int current_order = order;
 
-    // find a page from free_list, if any are available
-    free_block = free_list[order];
-
-    if (free_block != NULL) {
-        free_list[order] = free_block->next;
-        return free_block;
-    }
-
-    // no free blocks, we must create one from upper order by splitting
-    // find next biggest available block and start splitting it
-    for (int current_order = order; current_order <= MAX_ORDER; current_order++)
+    // find free block if any are available
+    while (current_order <= MAX_ORDER)
     {
-        free_block = free_list[current_order];
-
-        if (free_block == NULL)
+        if (free_list[current_order] != NULL)
         {
-            continue;
+            free_block = free_list[current_order];
+            free_list[current_order] = free_block->next;
         }
-
-        // we have a block lets split it until we have desired order
-        free_list[order] = free_block->next;
-
-
-
-
+        current_order++;
     }
 
+    // no block no memory
+    if (free_block == NULL)
+    {
+        return nullptr;
+    }
+
+    // split block until desired order is reached
+    // travels downwards from bigger order
+    while (current_order > order)
+    {
+        current_order--;
+
+        struct page *buddy = free_block + (1 << current_order);
+
+        buddy->order = current_order;
+        buddy->next = free_list[current_order];
+        free_list[current_order] = buddy;
+    }
+
+    free_block->order = order;
+    free_block->next = nullptr;
     return free_block;
 }
 
 void free_pages(struct page* page)
 {
+    if (page == NULL) return;
+
+    page->next = free_list[page->order];
+    free_list[page->order] = page;
 }
