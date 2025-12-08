1aa0ea6c981771b6acc4490df533451f57e78c31
miikaoskari <miika.karjalainen13@gmail.com>
2025-12-08 19:20:22 +0200

    add usage of logs and memory stuff

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <libfdt.h>
 #include "mos/driver.h"
+#include "mos/memory.h"
+#include "mos/log.h"
 
 volatile uint8_t *uart = (uint8_t *) 0x09000000;
 
@@ -19,8 +21,18 @@ void print(const char *s)
 void kmain(uint64_t dtb_ptr32, __unused uint64_t x1, __unused uint64_t x2, __unused uint64_t x3)
 {
     void *fdt = (void *)(uintptr_t)dtb_ptr32;
+
+    info("fdt pointer %p", fdt);
+
+    phys_mem_init(9, 9, 9, 9);
+    info("physical memory init done");
+
+    struct page *page = alloc_pages(0);
+
     register_all_drivers();
+
     probe_all_drivers_from_fdt(fdt);
+
 
     for(;;) {
         asm volatile("wfi");
