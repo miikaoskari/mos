3f21ad0250d51998198d90807c8baddce1c82f0c
miika <miika.karjalainen13@gmail.com>
2026-08-30 21:42:24 +0200

    Roll own types for kernel

---
+++ include/miosb/types.h
@@ -0,0 +1,23 @@
+#ifndef MIOSB_TYPES_H
+#define MIOSB_TYPES_H
+
+#include <stdint.h>
+
+typedef uint64_t u64;
+typedef int64_t s64;
+typedef uint32_t u32;
+typedef int32_t s32;
+typedef uint16_t u16;
+typedef int16_t s16;
+typedef uint8_t u8;
+typedef int8_t s8;
+typedef uintptr_t uptr;
+
+typedef unsigned char		uchar;
+typedef unsigned short		ushort;
+typedef unsigned int		uint;
+typedef unsigned long		ulong;
+typedef unsigned long long	ullong;
+
+
+#endif /* MIOSB_TYPES_H */
